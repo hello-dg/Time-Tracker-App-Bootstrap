c216e093d234b8806478ad0d776ec54a46abfbfe
David Green <hellodg.py@gmail.com>
2024-08-24 15:59:57 -0700

    Create separate search functions for each dropdown.

--- static/js/search.js
+++ static/js/search.js
@@ -1,10 +1,44 @@
-const searchInput = document.getElementById('search');
-const clientList = document.querySelectorAll('.search li');
+const categoryInput = document.getElementById('searchCategory');
+const categoryList = document.querySelectorAll('.searchCategory li');
 
-searchInput.addEventListener('input', () => {
-    const query = searchInput.value.toLowerCase();
+categoryInput.addEventListener('input', () => {
+    const query = categoryInput.value.toLowerCase();
 
-    clientList.forEach(item => {
+    categoryList.forEach(item => {
+        const clientName = item.textContent.toLowerCase();
+        if (clientName.includes(query)) {
+            item.style.display = '';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
+
+
+const projectInput = document.getElementById('searchProject');
+const projectList = document.querySelectorAll('.searchProject li');
+
+projectInput.addEventListener('input', () => {
+    const query = projectInput.value.toLowerCase();
+
+    projectList.forEach(item => {
+        const clientName = item.textContent.toLowerCase();
+        if (clientName.includes(query)) {
+            item.style.display = '';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
+
+
+const taskInput = document.getElementById('searchTask');
+const taskList = document.querySelectorAll('.searchTask li');
+
+taskInput.addEventListener('input', () => {
+    const query = taskInput.value.toLowerCase();
+
+    taskList.forEach(item => {
         const clientName = item.textContent.toLowerCase();
         if (clientName.includes(query)) {
             item.style.display = '';
