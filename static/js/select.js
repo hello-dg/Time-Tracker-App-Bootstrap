c94b430badf0fd4cb3132f56ec24d8734cae4fdf
David Green <hellodg.py@gmail.com>
2024-09-04 19:05:30 -0700

    Add dropdown selections to database

---
+++ static/js/select.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.category-item').forEach(function(item) {
+    item.addEventListener('click', function() {
+        document.getElementById('category-input').value = this.getAttribute('data-value');
+    });
+});
+
+document.querySelectorAll('.project-item').forEach(function(item) {
+    item.addEventListener('click', function() {
+        document.getElementById('project-input').value = this.getAttribute('data-value');
+    });
+});
+
+document.querySelectorAll('.task-item').forEach(function(item) {
+    item.addEventListener('click', function() {
+        document.getElementById('task-input').value = this.getAttribute('data-value');
+    });
+});
